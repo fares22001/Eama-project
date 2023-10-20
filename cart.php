c7364be3bc8db8609f23187d684495aa71d01a0e
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2023-10-20 19:31:44 +0300

    cart

---
+++ cart.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cart.css">
+    <title>In Cart Products</title>
+</head>
+<body>
+    <?php
+        include 'header.php'; 
+    ?>
+    <main>
+        <h1>0 Items</h1>
+        <hr>
+    </main>
+</body>
+</html>
