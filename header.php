0d4e5e44c3e85fd7c5f04542d2d188d47c7d65ee
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-10-20 18:05:58 +0300

    header

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <div class="logo">
+            <a href="index.php"><img src="logo.png" alt="" class="logo1"></a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
