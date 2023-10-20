acc3738e56a4b13f3c3b9ea46ac216bd04c134d3
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-10-20 23:28:10 +0300

    .

--- index.php
+++ index.php
@@ -6,56 +6,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beauty Products</title>
     <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/a98ac1f71c.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <?php include 'header.php'; ?>
 
-    <section class="hero">
-        <h1>Welcome to Our Beauty Products Store</h1>
-        <p>Discover the best in beauty and skincare.</p>
-        <a href="products.php" class="btn">Shop Now</a>
+    <section aria-label="Newest Photos">
+        <div class="carousel" data-carousel>
+            <button class="carousel-button prev" data-carousel-button="prev">&#8656;</button>
+            <button class="carousel-button next" data-carousel-button="next">&#8658;</button>
+            <ul data-slides>
+                <li class="slide" data-active>
+                    <div class="slide-text">
+                        <p>Beautiful Scenery</p>
+                    </div>
+                    <img src="https://source.unsplash.com/78A265wPiO4" alt="Nature Image #1">
+                </li>
+                <li class="slide">
+                    <div class="slide-text">
+                        <p>Nature's Beauty</p>
+                    </div>
+                    <img src="https://source.unsplash.com/eOpewngf68w" alt="Nature Image #2">
+                </li>
+                <li class="slide">
+                    <div class="slide-text">
+                        <p>Beautiful Scenery</p>
+                    </div>
+                    <img src="https://source.unsplash.com/ndN00KmbJ1c" alt="Nature Image #3">
+                </li>
+            </ul>
+        </div>
     </section>
 
-    <div class="slideshow-container">
-        <div class="mySlides fade">
-            <img src="image.jpg" alt="Image 1" class="slideimages">
-        </div>
 
-        <div class="mySlides fade">
-            <img src="image2.avif" alt="Image 2" class="slideimages">
-        </div>
+    <div>
+        <h1 class="feauteredproducts">feautered products</h1>
+    </div>
 
-        <div class="mySlides fade">
-            <img src="image3.webp" alt="Image 3" class="slideimages">
+    <div class="product-container">
+        <div class="product-card">
+            <img src="image.jpg" alt="Product 1" class="images">
+            <h3>Product 1</h3>
+            <p>Description of Product 1.</p>
+            <a href="#" class="btn">Buy Now</a>
         </div>
-
-        <div class="mySlides fade">
-            <img src="image4.webp" alt="Image 4" class="slideimages">
+        <div class="product-card">
+            <img src="image2.avif" alt="Product 2" class="images">
+            <h3>Product 2</h3>
+            <p>Description of Product 2.</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="product-card">
+            <img src="image3.webp" alt="Product 3" class="images">
+            <h3>Product 3</h3>
+            <p>Description of Product 3.</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="product-card">
+            <img src="image4.webp" alt="Product 4" class="images">
+            <h3>Product 4</h3>
+            <p>Description of Product 4.</p>
+            <a href="#" class="btn">Buy Now</a>
         </div>
     </div>
 
-    <section class="featured-products">
-        <h2>Featured Products</h2>
-        <div class="product">
-            <img src="image.jpg" alt="Product 1">
-            <h3>Silvercare</h3>
-            <p>Product 1 Description</p>
-            <a href="#">Shop Now</a>
-        </div>
-        <div class="product">
-            <img src="product2.jpg" alt="Product 2">
-            <h3>Product 2 Name</h3>
-            <p>Product 2 Description</p>
-            <a href="#">Shop Now</a>
-        </div>
-        <div class="product">
-            <img src="product3.jpg" alt="Product 3">
-            <h3>Product 3 Name</h3>
-            <p>Product 3 Description</p>
-            <a href="#">Shop Now</a>
-        </div>
-    </section>
+
+
+
+
+
+
 
     <!-- <section class="featured-products">
         <h2>Featured Products</h2>
@@ -82,6 +103,7 @@
 
     <?php include 'footer.php'; ?>
     <script src="slideshow.js"></script>
+    <script src="scroll.js"></script>
 </body>
 
 </html>
