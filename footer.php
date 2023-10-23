538219afbf59635bd65a950fa96ac1695e1b71b6
janaelmorshedy <janaelmorshedy20@gmail.com>
2023-10-23 17:30:05 +0300

    edit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+</head>
+
+<body>
+    <footer class="ecommerce-footer">
+        <div class="footer-content">
+            <div class="footer-section">
+                <h3>Contact Us</h3>
+                <p>Email: contact@yourstore.com</p>
+                <p>Phone: +1 (123) 456-7890</p>
+            </div>
+            <div class="footer-section">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-section">
+                <h3>Stay Connected</h3>
+                <p>Follow us on social media:</p>
+                <a href="#" class="social-icon"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="social-icon"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; 2023 YourStore. All rights reserved.
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+</head>
+
+<body>
+    <header class="ecommerce-header">
+        <div class="header-top">
+            <div class="logo">
+                <a href="index.php">Eama Group</a>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search products...">
+                <button><i class="fas fa-search"></i></button>
+            </div>
+            <div class="user-actions">
+                <a href="user login.php"><i class="fas fa-login"></i> login</a>
+                <a href="Register.php"><i class="fas fa-Register"></i> Register</a>
+                <a href="user/views/cart.php"><i class="fas fa-shopping-cart"></i> Cart</a>
+            </div>
+        </div>
+        <nav class="main-menu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
