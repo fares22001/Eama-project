ff2e4ea1b458307e8be78b7375438b88df19c5f0
Ahmed Ayman <ahmed2107685@miuegypt.edu.eg>
2023-11-22 18:26:51 +0200

    cart

--- config/connection.php
+++ config/connection.php
@@ -1,11 +1,10 @@
-<!-- <?php
+<?php
 
-        $server_name = "localhost";
-        $user_name = "root";
-        $password = "";
-        $db_name = "eama-project";
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_name = "eama-project";
 
-        $conn = new mysqli($server_name, $user_name, $password, $db_name);
+$conn = new mysqli($server_name, $user_name, $password, $db_name);
 
-
-        ?> -->
+?> 
