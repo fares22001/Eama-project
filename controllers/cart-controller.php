18fd9f375032820215dca054a3de13903828fe67
Ahmed Ayman <ahmed2107685@miuegypt.edu.eg>
2023-11-25 22:06:14 +0200

    table

--- controllers/cart-controller.php
+++ controllers/cart-controller.php
@@ -11,10 +11,12 @@ class Carts{
     }
 
     public function orders(){
-
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        
     }
 }
 $init = new Carts;
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     switch ($_POST['type']) {
         case 'orders':
