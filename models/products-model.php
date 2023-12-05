1e6fe3f220542df1e09fec02805bfd064e6dd77b
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-12-05 21:31:39 +0200

    .

--- models/products-model.php
+++ models/products-model.php
@@ -7,10 +7,10 @@ class product
     {
         $this->db = new Database();
     }
-    public function addproducts($data)
+    public function addproducts($data,$imagePath)
     {
         $this->db->query('INSERT INTO products (pname,pquantity,pdescription,pbrand,pcategory,pprice,psize,pimage)
-        VALUES(:name,:quantity,:description,:brand,:category,:price,:size,:image)');
+        VALUES(:name,:quantity,:description,:brand,:category,:price,:size,:pimage)');
         $this->db->bind(':name', $data['pname']);
         $this->db->bind(':quantity', $data['pquantity']);
         $this->db->bind(':description', $data['pdescription']);
@@ -18,20 +18,12 @@ class product
         $this->db->bind(':category', $data['pcategory']);
         $this->db->bind(':price', $data['pprice']);
         $this->db->bind(':size', $data['psize']);
-        $this->db->bind(':image', $data['pimage']);
+        $this->db->bind(':pimage', $imagePath);
         if ($this->db->execute()) {
             return true;
         } else {
             return false;
         }
-    }
-}
-class ProductModel
-{
-    private $db;
-    public function __construct()
-    {
-        $this->db = new Database();
     }
     public function getAllProducts()
     {
