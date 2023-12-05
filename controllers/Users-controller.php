d97ae13ca5261d37c9cb7d348f28d5cf014e60d4
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-12-05 21:31:37 +0200

    .

--- controllers/Users-controller.php
+++ controllers/Users-controller.php
@@ -1,127 +1,134 @@
 <?php
 
-    require_once '../models/User.php';
-    require_once '../helpers/session-helper.php';
-    require_once '../libraries/Database.php';
+require_once '../models/User.php';
+require_once '../helpers/session-helper.php';
+require_once '../libraries/Database.php';
 
 
-    class Users {
+class Users
+{
 
-        private $userModel;
-        
-        public function __construct(){
-            $this->userModel = new User;
-        }
+    private $userModel;
 
-        public function register(){
-            //Process form
-            
-            //Sanitize POST data
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    public function __construct()
+    {
+        $this->userModel = new User;
+    }
 
-            //Init data
-            $data = [
-                'UsersName' => trim($_POST['UsersName']),
-                'UsersEmail' => trim($_POST['UsersEmail']),
-                'UsersUid' => trim($_POST['UsersUid']),
-                'UsersPwd' => trim($_POST['UsersPwd']),
-                'pwdRepeat' => trim($_POST['pwdRepeat']),
-                'UsersRole' => trim($_POST['UsersRole'])
-            ];
-            
+    public function register()
+    {
+        //Process form
 
-            // if ($this->userModel->register($data)) {
-            //     redirect("../views/Register.php");
-            // } else {
-            //     die("something went wrong");
-            // }
-
-            //Validate inputs
-            if(empty($data['UsersName']) || empty($data['UsersEmail']) || empty($data['UsersUid']) || 
-            empty($data['UsersPwd']) || empty($data['pwdRepeat'])){
-                flash("register", "Please fill out all inputs");
-                redirect("../views/Rgeister.php");
-            }
-
-            if(!preg_match("/^[a-zA-Z0-9]*$/", $data['UsersUid'])){
-                flash("register", "Invalid username");
-                redirect("../views/Register.php");
-            }
-
-            if(!filter_var($data['UsersEmail'], FILTER_VALIDATE_EMAIL)){
-                flash("register", "Invalid email");
-                redirect("../views/Register.php");
-            }
-
-            if(strlen($data['UsersPwd']) < 6){
-                flash("register", "Invalid password");
-                redirect("../views/Register.php");
-            } else if($data['UsersPwd'] !== $data['pwdRepeat']){
-                flash("register", "Passwords don't match");
-                redirect("../views/Register.php");
-            }
-
-            // User with the same email or password already exists
-            if($this->userModel->findUserByEmailOrUsername($data['UsersEmail'], $data['UsersName'])){
-                flash("register", "Username or email already taken");
-                redirect("../views/Register.php");
-            }
-
-            //Passed all validation checks.
-            //Now going to hash password
-            $data['UsersPwd'] = password_hash($data['UsersPwd'], PASSWORD_DEFAULT);
-
-            //Register User
-            if($this->userModel->register($data)){
-                // redirect("../views/user login.php");
-                echo "done";
-            }else{
-                die("Something went wrong");
-            }
-            
-        }
-
-    public function login(){
         //Sanitize POST data
         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
         //Init data
-        $data=[
+        $data = [
+            'UsersName' => trim($_POST['UsersName']),
+            'UsersEmail' => trim($_POST['UsersEmail']),
+            'UsersUid' => trim($_POST['UsersUid']),
+            'UsersPwd' => trim($_POST['UsersPwd']),
+            'pwdRepeat' => trim($_POST['pwdRepeat']),
+            'UsersRole' => trim($_POST['UsersRole'])
+        ];
+
+
+        // if ($this->userModel->register($data)) {
+        //     redirect("../views/Register.php");
+        // } else {
+        //     die("something went wrong");
+        // }
+
+        //Validate inputs
+        if (
+            empty($data['UsersName']) || empty($data['UsersEmail']) || empty($data['UsersUid']) ||
+            empty($data['UsersPwd']) || empty($data['pwdRepeat'])
+        ) {
+            flash("register", "Please fill out all inputs");
+            redirect("../views/Rgeister.php");
+        }
+
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $data['UsersUid'])) {
+            flash("register", "Invalid username");
+            redirect("../views/Register.php");
+        }
+
+        if (!filter_var($data['UsersEmail'], FILTER_VALIDATE_EMAIL)) {
+            flash("register", "Invalid email");
+            redirect("../views/Register.php");
+        }
+
+        if (strlen($data['UsersPwd']) < 6) {
+            flash("register", "Invalid password");
+            redirect("../views/Register.php");
+        } else if ($data['UsersPwd'] !== $data['pwdRepeat']) {
+            flash("register", "Passwords don't match");
+            redirect("../views/Register.php");
+        }
+
+        // User with the same email or password already exists
+        if ($this->userModel->findUserByEmailOrUsername($data['UsersEmail'], $data['UsersName'])) {
+            flash("register", "Username or email already taken");
+            redirect("../views/Register.php");
+        }
+
+        //Passed all validation checks.
+        //Now going to hash password
+        $data['UsersPwd'] = password_hash($data['UsersPwd'], PASSWORD_DEFAULT);
+
+        //Register User
+        if ($this->userModel->register($data)) {
+            // redirect("../views/user login.php");
+            echo "done";
+        } else {
+            die("Something went wrong");
+        }
+    }
+
+    public function login()
+    {
+        //Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        //Init data
+        $data = [
             'UsersUid/UsersEmail' => trim($_POST['UsersUid/UsersEmail']),
             'UsersPwd' => trim($_POST['UsersPwd'])
         ];
 
-        if(empty($data['UsersUid/UsersEmail']) || empty($data['UsersPwd'])){
+        if (empty($data['UsersUid/UsersEmail']) || empty($data['UsersPwd'])) {
             flash("login", "Please fill out all inputs");
             header("location: ../views/user login.php");
             exit();
         }
 
         //Check for user/email
-        if($this->userModel->findUserByEmailOrUsername($data['UsersUid/UsersEmail'], $data['UsersUid/UsersEmail'])){
+        if ($this->userModel->findUserByEmailOrUsername($data['UsersUid/UsersEmail'], $data['UsersUid/UsersEmail'])) {
             //User Found
             $loggedInUser = $this->userModel->login($data['UsersUid/UsersEmail'], $data['UsersPwd']);
-            if($loggedInUser){
+            if ($loggedInUser) {
                 //Create session
                 $this->createUserSession($loggedInUser);
-            }else{
+            } else {
                 flash("login", "Password Incorrect");
                 redirect("../views/user login.php");
             }
-        }else{
+        } else {
             flash("login", "No user found");
             redirect("../views/user login.php");
         }
     }
 
-    public function createUserSession($user){
+    public function createUserSession($user)
+    {
         $_SESSION['UsersId'] = $user->usersId;
         $_SESSION['UsersName'] = $user->usersName;
         $_SESSION['UsersEmail'] = $user->usersEmail;
-        redirect("../views/views/Userprofile.php");
+        redirect("../views/Userprofile.php");
     }
 
-    public function logout(){
+    public function logout()
+    {
         unset($_SESSION['UsersId']);
         unset($_SESSION['UsersName']);
         unset($_SESSION['UsersEmail']);
@@ -130,29 +137,26 @@
     }
 }
 
-    $init = new Users;
+$init = new Users;
 
-    //Ensure that user is sending a post request
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        switch($_POST['type']){
-            case 'register':
-                $init->register();
-                break;
-            case 'login':
-                $init->login();
-                break;
-            default:
+//Ensure that user is sending a post request
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    switch ($_POST['type']) {
+        case 'register':
+            $init->register();
+            break;
+        case 'login':
+            $init->login();
+            break;
+        default:
             redirect("../views/index.php");
-        }
-        
-    }else{
-        switch($_GET['q']){
-            case 'logout':
-                $init->logout();
-                break;
-            default:
-            redirect("../views/index.php");
-        }
     }
-
-    
+} else {
+    switch ($_GET['q']) {
+        case 'logout':
+            $init->logout();
+            break;
+        default:
+            redirect("../views/index.php");
+    }
+}
