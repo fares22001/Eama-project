0acb175f4738679856d6525acd9b2505a8a9c103
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-12-05 21:31:47 +0200

    .

--- views/Userprofile.php
+++ views/Userprofile.php
@@ -1,14 +1,16 @@
 <?php include 'header.php';
-    include_once '../helpers/session-helper.php';
-    session_start(); 
-    ?>
- 
+include_once '../helpers/session-helper.php';
+include_once '../models/User.php';
+?>
 
-<h1 id="index-text">Welcome, <?php if(isset($_SESSION['usersId'])){
-        echo explode(" ", $_SESSION['usersName'])[0];
-    }else{
-        echo 'Guest';
-    } 
-    ?> </h1>
-    <a href="../controllers/Users-controller.php?q=logout"><li>Logout</li></a>
-     <?php include 'footer.php'; ?>
+
+<h1 id="index-text">Welcome, <?php if (isset($_SESSION['UsersId'])) {
+                                    echo explode(" ", $_SESSION['UsersName'])[0];
+                                } else {
+                                    echo 'Guest';
+                                }
+                                ?> </h1>
+<a href="../controllers/Users-controller.php?q=logout">
+    <li>Logout</li>
+</a>
+<?php include 'footer.php'; ?>
