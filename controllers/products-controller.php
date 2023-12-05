658e730b3a5ac00da0b0968a1ad7eab82f89ad52
fares abdelhamid <fares2100380@miuegypt.edu.eg>
2023-12-05 21:31:34 +0200

    .

--- controllers/products-controller.php
+++ controllers/products-controller.php
@@ -3,17 +3,46 @@ require_once '../models/products-model.php';
 require_once '../helpers/session-helper.php';
 class products
 {
-    private $userModel;
+    private $productModel;
     public function __construct()
     {
-        $this->userModel = new product();
+        $this->productModel = new product();
     }
+
+    private function saveImage($file)
+    {
+        // Check if the file is set and not empty
+        if (!isset($file) || empty($file)) {
+            // File wasn't inputted in the form
+
+            return false;
+        }
+
+        $uploadDir = '../public/img/';
+        $imageFileType = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+
+        // Generate a unique name for the image
+        $imageName = uniqid('image_') . '.' . $imageFileType;
+        $targetPath = $uploadDir . $imageName;
+
+        // Create the category subfolder if it doesn't exist
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0755);
+        }
+
+        // Move the uploaded file to the destination subfolder
+        if (move_uploaded_file($file['tmp_name'], $targetPath))
+            return $targetPath;
+        else
+            return false;
+    }
+
     public function addproducts()
     {
         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $selectedCategory = $_POST['pcategory'];
         $data = [
-           
+
             'pname' => trim($_POST['pname']),
             'pquantity' => trim($_POST['pquantity']),
             'pdescription' => trim($_POST['pdescription']),
@@ -21,7 +50,6 @@ class products
             'pcategory' => trim($_POST['pcategory']),
             'pprice' => trim($_POST['pprice']),
             'psize' => trim($_POST['psize']),
-            'pimage' => trim($_POST['pimage'])
         ];
         if (
             empty($data['pname']) || empty($data['pquantity']) || empty($data['pdescription']) || empty($data['pbrand']) ||
@@ -34,33 +62,13 @@ class products
             flash("addproducts", "Max 10 products");
             redirect("../admin/product-create.php");
         }
-        if ($this->userModel->addproducts($data)) {
+        $imagePath = $this->saveImage($_FILES['pimage']);
+        if ($this->productModel->addproducts($data, $imagePath)) {
             redirect("../admin/product-create.php");
         } else {
             die("something went wrong");
         }
     }
-}
-$init = new products;
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    switch ($_POST['type']) {
-        case 'addproducts':
-            $init->addproducts();
-            break;
-    }
-}
-
-
-class ProductController
-{
-    private $productModel;
-
-    public function __construct()
-    {
-        require_once '../controllers/products-controller.php';
-        $this->productModel = new ProductModel();
-    }
-
     public function getAllProducts()
     {
         $products = $this->productModel->getAllProducts();
@@ -71,5 +79,14 @@ class ProductController
         } else {
             echo 'Error retrieving products.';
         }
+    }
+}
+
+$init = new products;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    switch ($_POST['type']) {
+        case 'addproducts':
+            $init->addproducts();
+            break;
     }
 }
